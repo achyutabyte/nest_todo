58c76ba4ecd1ff3986cc6282385cef9dc48fba13
Achyuta12345 <panda9191achyuta@gmail.com>
2026-07-14 10:23:22 +0530

    feat: implement tasks module with controller and service and remove boilerplate app code

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { get } from 'http';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private tasksService: TasksService) {
+
+    }
+
+    @Get()
+    getAllTasks() {
+        return this.tasksService.getAllTasks();
+    }
+}
